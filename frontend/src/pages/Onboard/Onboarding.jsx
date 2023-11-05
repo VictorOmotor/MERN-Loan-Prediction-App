2f7d23716aa13f82ffa8052a16f342bfd2ba33a0
omotorvidosky <omotorvidosky@gmail.com>
2023-11-05 09:26:04 +0100

    Created the Onboarding page UI

--- frontend/src/pages/Onboard/Onboarding.jsx
+++ frontend/src/pages/Onboard/Onboarding.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import BgImg from '../../assets/images/BgImg.png'
+import { LoginButton } from '../../components/Buttons/Buttons'
 
 const Onboarding = () => {
   return (
-    <div>Onboarding</div>
+    <div>
+      <img src={BgImg} className='w-screen h-screen fixed' alt="background" />
+      <ul className=' relative uppercase text-white font-[Inter] leading-9 
+      tracking-[-0.00119rem] font-semibold text-[1.19588rem] flex justify-end gap-6
+      py-16 px-24'>
+        <li className='cursor-pointer'>Login</li>
+        <li className='cursor-pointer'>Sign Up</li>
+      </ul>
+    </div>
   )
 }
 
